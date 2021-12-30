cd7f185eb745c268993b1aa9ae132a92345a76b4
zqlzll <73019368+zqlzll@users.noreply.github.com>
2021-12-30 15:20:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+document.querySelector('html').onclick = function() {
+    alert('别戳我，我怕疼。');
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+ }
